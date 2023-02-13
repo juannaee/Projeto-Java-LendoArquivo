ad8e99f3831c60c0a7a5068bb50466db91ce76e1
Juan Guilherme Silva Lemos <juangsilvalemos@gmail.com>
2023-02-13 20:31:32 -0300

    Alterações feitas em casa(Adicionei novas funcionalidades em manipulação de arquivos)

--- src/application/ManipulandoPastas.java
+++ src/application/ManipulandoPastas.java
@@ -19,21 +19,44 @@ public class ManipulandoPastas {
 		File[] folders = path.listFiles(File::isDirectory);
 
 		System.out.println("Pastas: ");
-
+		System.out.println("---------------------------------------");
 		for (File folder : folders) {
+	
 			System.out.println(folder);
+			
 		}
+		System.out.println("---------------------------------------");
+		System.out.println("---------------------------------------");
 
 		// Pegando uma lista de todos os arquivos dentro da pasta(diretorio)
 		File[] files = path.listFiles(File::isFile);
 		System.out.println("Arquivos: ");
+		System.out.println("---------------------------------------");
 		for (File file : files) {
+	
 			System.out.println(file);
+		
 		}
+		System.out.println("---------------------------------------");
 
+		System.out.println("---------------------------------------");
 		boolean sucess = new File(strPath + "\\subPasta").mkdir();
 		System.out.println("A subpasta foi criada? : " + sucess);
+		System.out.println("---------------------------------------");
 
+		
+		
+		//Pegando nome ou caminho de arquivos; 
+		System.out.println("--------------------------------------------------------------");
+		System.out.println("---------------------------------------");
+		System.out.println("Nome da pasta");
+		System.out.println(path.getName());
+		System.out.println("---------------------------------------");
+		System.out.println("Cmainho do diretorio");
+		System.out.println(path.getParent());
+		System.out.println("--------------------------------------------------------------");
+		
+		
 		sc.close();
 
 	}
